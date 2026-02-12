5eb4581b425566d9a6aae6497d188a8cf73ac216
ThoraDis <142800101+ThoraDis@users.noreply.github.com>
2026-02-12 19:50:27 +0000

    create: test case

---
+++ dist/lib/db.test.js
@@ -0,0 +1,13 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+import { deleteTodo, updateTodo } from './db.js';
+describe('db', () => {
+    describe('deleteTodo', () => {
+        it('should fail on non object input', () => {
+        });
+    });
+    describe('updateTodo', () => {
+        it('should fail on non object input', () => {
+        });
+    });
+});
